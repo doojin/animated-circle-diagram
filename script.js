d7683b76f5d87e73054d0d21a79ed5c0ab7d24c2
Dmitry Papka <dmitry.papka@gmail.com>
2019-01-13 13:46:42 -0300

    Adding note value

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ displays.forEach(display => {
     const radius = progress.r.baseVal.value;
     const circumference = 2 * Math.PI * radius;
 
+    const note = parseFloat(display.dataset.note);
+    const offset = circumference * (10 - note) / 10;
+
     progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
     progress.style.setProperty('--initialStroke', circumference);
 
-    setTimeout(() => progress.style.strokeDashoffset = 50, 100);
+    setTimeout(() => progress.style.strokeDashoffset = offset, 100);
 });
