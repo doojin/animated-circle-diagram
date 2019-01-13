4ed592d914aed5f83b5fe963f05726a1361fc532
Dmitry Papka <dmitry.papka@gmail.com>
2019-01-13 13:47:52 -0300

    Refactoring javascript code

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const displays = document.querySelectorAll('.note-display');
 const transitionDuration = 900;
 
 displays.forEach(display => {
+    const note = parseFloat(display.dataset.note);
+    strokeTransition(display, note);
+});
+
+function strokeTransition(display, note) {
     const progress = display.querySelector('.circle__progress--fill');
     const radius = progress.r.baseVal.value;
     const circumference = 2 * Math.PI * radius;
-
-    const note = parseFloat(display.dataset.note);
     const offset = circumference * (10 - note) / 10;
 
     progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
     progress.style.setProperty('--initialStroke', circumference);
 
     setTimeout(() => progress.style.strokeDashoffset = offset, 100);
-});
+}
